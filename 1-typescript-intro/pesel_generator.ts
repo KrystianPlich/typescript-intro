4fdcbbcf2973b8006f7865a9336d04ae03fa58eb
Krystian Plich <KrystianPlich@github.com>
2021-08-12 11:33:22 +0200

    Task1 - Using moments module to format pesel date

--- 1-typescript-intro/pesel_generator.ts
+++ 1-typescript-intro/pesel_generator.ts
@@ -1,8 +1,11 @@
-function peselGen(birthDate: string) {
+import * as moment from 'moment'
+
+function peselGen(birthDate: Date) {
     let pesel: string[] = [];
     const multipliedBy: number[] = [1,3,7,9,1,3,7,9,1,3];
-    let newDate: string = birthDate.slice(8) + birthDate.slice(3,5) + birthDate.slice(0,2);
-    let dateArray: string[] = newDate.split("");
+    // let newDate: string = birthDate.slice(8) + birthDate.slice(3,5) + birthDate.slice(0,2);
+    let newDate = moment(birthDate).format('YY-MM-DD');
+    let dateArray: string[] = newDate.split("-");
     pesel.push(...dateArray)
     let x: string[] = (Math.floor(Math.random()*9000) + 1000).toString().split("");
     pesel.push(...x);
@@ -16,5 +19,4 @@ function peselGen(birthDate: string) {
     pesel.push(checkSum.toString());
     return pesel.join("");
 }
-
 export { peselGen }
